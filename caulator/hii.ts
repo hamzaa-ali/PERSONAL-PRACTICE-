ef552ac07c50f322cf1a56e2453a871c141004b8
ALI HAMZA NADEEM RANA <160017611+hamzaa-ali@users.noreply.github.com>
2024-03-06 17:06:24 +0500

    Add files via upload

---
+++ caulator/hii.ts
@@ -0,0 +1,19 @@
+let a: number=5
+let b: number=3
+console.log("a+b",a+b)
+console.log("a-b",a-b)
+console.log("a/b",a/b)
+console.log("a%b",a%b)
+console.log("a*b",a**b)
+
+let A: number=10
+let B: number=20
+console.log("a+b",A+B)
+console.log("a-multiplicationb-A**b")
+
+let total:number=20
+let summ:Number=total+10
+console.log("total",+summ)
+
+
+
