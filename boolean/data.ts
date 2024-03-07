247bc130ed29e8a32d8341cd429bb409791695d5
ALI HAMZA NADEEM RANA <160017611+hamzaa-ali@users.noreply.github.com>
2024-03-07 15:10:04 +0500

    Add files via upload

---
+++ boolean/data.ts
@@ -0,0 +1,13 @@
+function iseven(number1:number ):boolean{
+return number1  %2===0
+}
+console.log(iseven(50));
+console.log(iseven(100))
+console.log(iseven(95))
+
+function isodd(nono:number ):boolean{
+    return nono  %5===0
+    }
+    console.log(isodd(50));
+    console.log(isodd(9));
+    console.log(isodd(0));
