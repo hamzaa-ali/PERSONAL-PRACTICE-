4a2798149a115ca23759f60120902816fbe4158f
ALI HAMZA NADEEM RANA <160017611+hamzaa-ali@users.noreply.github.com>
2024-03-07 14:53:44 +0500

    Add files via upload

---
+++ array/do.ts
@@ -0,0 +1,16 @@
+// Declaring an array of numbers
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+// Declaring an array of strings
+let fruits: string[] = ['apple', 'banana', 'orange'];
+
+// Accessing elements of the arrays
+console.log(numbers[0],numbers[3]); // Output: 1 and 4
+console.log(fruits[1]); // Output: banana
+
+// Modifying elements of the arrays
+numbers[2] = 10;
+fruits[0] = 'pear';
+console.log(numbers,fruits)
+
+
