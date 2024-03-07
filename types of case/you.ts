a6de3d7f5fde3187e52d3c61d7027d79769262b4
ALI HAMZA NADEEM RANA <160017611+hamzaa-ali@users.noreply.github.com>
2024-03-07 14:55:16 +0500

    Add files via upload

---
+++ types of case/you.ts
@@ -0,0 +1,17 @@
+//camelCase
+//snake_case
+//PascalCase
+
+let userName:string="hello"
+//cameCase in this case 2nd alphabet of variable always capital
+
+let user_name:string="hello"
+//snake_case inthis case we use underscore ( _ ) b/w 2 variable name. as alternative (sapce)
+
+let UserName:string="hello"
+//PascalCase in this case we use  capital letter of 1st alphabet and also 2nd alphabetof variable OR word
+
+let UserNameIs:string="hello" 
+// use camelCase if the vaiable name  more then 2 or 3 words.
+
+console.log(userName ,""+ user_name, "" + UserName, "" + UserNameIs);
